2f282aca083424c551aa052da3da6dfceb175d0b
Rmoran95 <rmoran8@uic.edu>
2017-11-08 19:17:35 -0600

    GUI is being built

---
+++ src/GUI.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.lang.*;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+
+public class GUI extends JFrame implements ActionListener{
+    private
+
+    public GUI(){
+        super("Battleship");
+        getContentPane().setBackground(Color.gray);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
